ae34f93e7a3fb9281772d9c349c565f1b4c33f16
rdzeeuw <77979140+rdzeeuw@users.noreply.github.com>
2021-04-08 10:44:49 +0200

    Add files via upload

---
+++ JS-runnen/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten");
+
+//deel 2
+//dit is een grote som
+/*console.log(1230941 * 1823796); // = 2244985272036
+console.log(637263 / 54); // = 11801.166666666666 */
